d9310d24fee51dd53e26f8d3090ff590d9392785
Daleniant <vladusinskm@gmail.com>
2023-01-22 04:59:15 -0800

    Added epoch time at init for thread
    Now thread gets epoch at init, saves and loads it from file

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -16,10 +16,44 @@ function receive() {
 		console.log('Tried fetch.')
 
 		if (response.ok)
-			console.log('Sent JSON to server.')
+			console.log('Sent JSON to server.');
 		else
-			console.log('Couldn\'t send JSON to server.')
+			console.log('Couldn\'t send JSON to server.');
 	});
 
 	console.log('After fetch')
+}
+
+
+function load_path (var path, var index) {
+	var data = {
+		type: "list";
+		cmd: "list " + path + "/" + index;
+	};
+
+	console.log(window.location.href)
+
+	fetch (window.location.href, {
+		headers : {
+			'Content-Type' : 'application/json'
+		},
+		method : 'POST',
+		body : JSON.stringify (data),
+	})
+	.then (function (response) {
+		console.log('Tried fetch.')
+
+		if (response.ok) {
+			response.json()
+			.then (function (response) {
+				
+			});
+		}
+		else {
+			console.log('Couldn\'t send JSON to server.');
+		}
+	})
+	.catch (function (error) {
+		console.log(error);
+	});
 }
