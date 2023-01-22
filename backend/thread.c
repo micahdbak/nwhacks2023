debaf1b36065cd763f691fe77ccddb180fc0d8a9
Daleniant <vladusinskm@gmail.com>
2023-01-21 17:18:47 -0800

    Updated threads
    Added score thing to threads and sorting by different methods

--- backend/thread.c
+++ backend/thread.c
@@ -12,6 +12,7 @@ node_t* create_node (thread* _thr, node_t* _next) {
     return new_node;
 }
 
+// Forward declaration
 void delete_thread (thread** thr);
 
 void delete_node (node_t** node) {
@@ -30,10 +31,22 @@ void delete_node (node_t** node) {
 
 
 ll_t* create_list (thread* thr) {
-    ll_t* new_list = (ll_t*)malloc(sizeof(new_list));
+    ll_t* new_list = (ll_t*)malloc(sizeof(ll_t));
     new_list->head = create_node (thr, NULL);
 
     return new_list;
+}
+
+ll_t* deep_copy (ll_t* list) {
+    ll_t* copy = (ll_t*)malloc(sizeof(ll_t));
+    
+    node_t* cur = list->head;
+    while (cur != NULL) {
+        add_to_list (copy, cur->thr);
+        cur = cur->next;
+    }
+
+    return copy;
 }
 
 void add_to_list (ll_t* list, thread* _thr) {
@@ -59,10 +72,11 @@ void delete_list (ll_t** list) {
 // Functions for Thread struct
 // ************************************
 
-thread* create_thread (char _content[256], char _author[32], int _date[3]) {
+thread* create_thread (char _content[1024], char _author[32], int _date[3]) {
     thread* thr = (thread*)malloc(sizeof(thread));
     thr->n = 0;
     thr->sub_threads = NULL;
+    thr->score = 0;
 
     int i;
     for (i = 0; _content[i] != '\0'; i++)
@@ -73,7 +87,7 @@ thread* create_thread (char _content[256], char _author[32], int _date[3]) {
         thr->author[i] = _author[i];
     thr->author[i] = '\0';
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 4; i++)
         thr->date[i] = _date[i];
 
     return thr;
@@ -87,6 +101,22 @@ void add_subthread (thread* thr, thread* sub_thr) {
     else 
         add_to_list (thr->sub_threads, sub_thr);
 }
+
+
+void add_score (thread* thr, bool like) {
+    thr->score += (like) ? 1 : -1;
+}
+
+ll_t* sort_by (thread* thr, int (*cmpfunc) (const void*, const void*)) {
+    if (thr->sub_threads == NULL)
+        return;
+
+    ll_t* copy = deep_copy (thr->sub_threads);
+    qsort (copy, thr->n, sizeof(node_t), cmpfunc);
+
+    return copy;
+}
+
 
 void remove_thread (thread* thr) {
     strncpy(thr->content, "This post has been deleted by author", 256);
@@ -102,9 +132,45 @@ void delete_thread (thread** thr) {
 }
 
 
+// ************************************
+// Functions for Sorting threads
+// ************************************
+
+int by_likes (const void* a, const void* b) {
+    const thread* thr_a = (thread*) a;
+    const thread* thr_b = (thread*) b;
+
+    return thr_b->score - thr_a->score;
+}
+
+int by_date (const void* a, const void* b) {
+    const thread* thr_a = (thread*) a;
+    const thread* thr_b = (thread*) b;
+
+    // Skip to month if years are the same
+    if (thr_b->date[0] != thr_a->date[0])
+        return thr_b->date[0] - thr_a->date[0];
+    // Skip to day if months are the same
+    if (thr_b->date[1] != thr_a->date[1])
+        return thr_b->date[1] - thr_a->date[1];
+    // Skip to seconds if days are the same
+    if (thr_b->date[2] != thr_a->date[2])
+        return thr_b->date[2] - thr_a->date[2];
+    // Compare by seconds if all other values are the same
+    return thr_b->date[3] - thr_a->date[3];
+}
+
+int by_comments (const void* a, const void* b) {
+    const thread* thr_a = (thread*) a;
+    const thread* thr_b = (thread*) b;
+
+    return thr_b->n - thr_a->n;
+}
+
+
 int main () {
-    int _date[3] = {10, 1, 2};
-    char _content[256] = "AUHEUFBEIE";
+    int _date[4] = {10, 1, 2, 1000};
+    char _content[1024] = "AUHEUFBEIE";
     char _author[32] = "WNIUFJFJWNEO";
 
     thread* thr = create_thread (_content, _author, _date);
--- backend/thread.h
+++ backend/thread.h
@@ -29,6 +29,7 @@ typedef struct {
 } ll_t;
 
 ll_t* create_list (thread* thr);
+ll_t* deep_copy (ll_t* list);       // Deep copies the list itself without deepcopying nodes
 void add_to_list (ll_t* list, thread* _thr);
 void delete_list (ll_t** list);
 
@@ -37,14 +38,25 @@ struct Thread {
     int n;              // Number of directly linked subthreads
     ll_t* sub_threads;  // Pointers to those subthreads
     enum thread_type type;
-    char content[256];  // Content of the thread
-    char author[32];    // Author of the post
-    int date[3];        // year/month/day
-};
-//typedef struct Thread thread;
 
-thread* create_thread (char _content[256], char _author[32], int _date[3]);
+    char content[1024];  // Content of the thread
+    char author[32];    // Author of the post
+    int date[4];        // year/month/day/seconds since beginning of day
+    int score;
+};
+
+thread* create_thread (char _content[1024], char _author[32], int _date[4]);
 void add_subthread (thread* thr, thread* sub_thr);
+
+void add_score (thread* thr, bool like);
+
+// Comparison functions for sorting
+int by_likes (const void* a, const void* b);    // Sort by score in descending order
+int by_date (const void* a, const void* b);     // Sort by date in descending order (new first)
+int by_comments (const void* a, const void* b); // Sort by number of comments in descending order
+
+// Given the comparison function, sort the sub-threads of given thread
+ll_t* sort_by (thread* thr, int (*cmpfunc) (const void*, const void*));
 
 void remove_thread (thread* thr); // soft delete
 void delete_thread (thread** thr); // hard delete
