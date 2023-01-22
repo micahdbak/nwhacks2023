00b513da9aed83035a2dc7cad7c8c5c6ab85aaf9
Daleniant <vladusinskm@gmail.com>
2023-01-22 06:19:21 -0800

    Updated Thread in js
    Now Thread object also stores it's path

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -1,5 +1,7 @@
 function post() {
 	var data = {
+		type: "post",
+		cmd: document.getElementById("1").value
 	};
 
 	fetch(window.location.href, {
@@ -20,11 +22,12 @@ function post() {
 
 // Rerpresents the same thread as in backend
 class Thread {
-	constructor (_type, _content, _author, _epoch) {
+	constructor (_type, _content, _author, _epoch, _path) {
 		this.type    = _type;
 		this.content = _content;
 		this.author  = _author;
 		this.epoch   = _epoch;
+		this.path = _path;
 	}
 	
 	get type () {
@@ -40,25 +43,39 @@ class Thread {
 	}
 	
 	get epoch () {
-		retrun this.epoch;
+		return this.epoch;
+	}
+	
+	get path () {
+		return this.path;
 	}
 }
 
 // Converts a string of format type~content~author~epoch into thread object
-function string_to_thread (var str) {
+function string_to_thread (var str, var path) {
 	const parts = text.split ("~");
 	
-	const thr = new Thread (parseInt(parts[0]), parts[1], parts[2], parseInt(parse[3]));
+	const thr = new Thread (parseInt(parts[0]), 
+							parts[1], parts[2], 
+							parseInt(parse[3]),
+							path);
 	
 	return thr;
 }
 
+// Retrieves the threads from requested directory
 function load_path (var path, var index) {
+	var full_path = "list " + path + "/" + index;
+	
 	var data = {
+		type: "list",
 		// Command for python socket to run
+		cmd: full_path
 	};
 
 	console.log(window.location.href)
+	
+	// Array of Thread objects that represents this directory to-load
 	const threads = [];
 
 	fetch (window.location.href, {
@@ -78,7 +95,7 @@ function load_path (var path, var index) {
 				const lines = response.threads.split("\n");
 				
 				for (var line : lines) {
-					threads.push (string_to_thread (line));
+					threads.push (string_to_thread (line), full_path);
 				}
 			});
 		}
